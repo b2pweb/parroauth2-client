4187fb19ff29a299e3ef3289ffa65d1b94dfa92c
Sébastien <stanneux@b2pweb.com>
2023-07-25 11:29:07 +0200

    tests: Update dependencies on psr7 and bshaffer to fix tests

--- tests/lib/TestingDataSet.php
+++ tests/lib/TestingDataSet.php
@@ -26,13 +26,79 @@ class TestingDataSet
             return $this;
         }
 
-        $this->connection->exec('CREATE TABLE oauth_clients (client_id TEXT, client_secret TEXT, redirect_uri TEXT)');
-        $this->connection->exec('CREATE TABLE oauth_access_tokens (access_token TEXT, client_id TEXT, user_id TEXT, expires TIMESTAMP, scope TEXT)');
-        $this->connection->exec('CREATE TABLE oauth_authorization_codes (authorization_code TEXT, client_id TEXT, user_id TEXT, redirect_uri TEXT, expires TIMESTAMP, scope TEXT, id_token TEXT)');
-        $this->connection->exec('CREATE TABLE oauth_refresh_tokens (refresh_token TEXT, client_id TEXT, user_id TEXT, expires TIMESTAMP, scope TEXT)');
-        $this->connection->exec('CREATE TABLE oauth_scopes (scope TEXT, is_default BOOLEAN);');
-        $this->connection->exec('CREATE TABLE oauth_users (username VARCHAR(255) NOT NULL, password VARCHAR(2000), email VARCHAR(255) DEFAULT NULL, name VARCHAR(255) DEFAULT NULL, family_name VARCHAR(255) DEFAULT NULL, CONSTRAINT username_pk PRIMARY KEY (username));');
-        $this->connection->exec('CREATE TABLE oauth_public_keys (client_id VARCHAR(80), public_key VARCHAR(8000), private_key VARCHAR(8000), encryption_algorithm VARCHAR(80) DEFAULT "RS256")');
+        $sql = "
+            CREATE TABLE oauth_clients (
+              client_id             VARCHAR(80)   NOT NULL,
+              client_secret         VARCHAR(80),
+              redirect_uri          VARCHAR(2000),
+              grant_types           VARCHAR(80),
+              scope                 VARCHAR(4000),
+              user_id               VARCHAR(80),
+              PRIMARY KEY (client_id)
+            );
+
+            CREATE TABLE oauth_access_tokens (
+              access_token         VARCHAR(40)    NOT NULL,
+              client_id            VARCHAR(80)    NOT NULL,
+              user_id              VARCHAR(80),
+              expires              TIMESTAMP      NOT NULL,
+              scope                VARCHAR(4000),
+              PRIMARY KEY (access_token)
+            );
+
+            CREATE TABLE oauth_authorization_codes (
+              authorization_code  VARCHAR(40)    NOT NULL,
+              client_id           VARCHAR(80)    NOT NULL,
+              user_id             VARCHAR(80),
+              redirect_uri        VARCHAR(2000),
+              expires             TIMESTAMP      NOT NULL,
+              scope               VARCHAR(4000),
+              id_token            VARCHAR(1000),
+              code_challenge        VARCHAR(1000),
+              code_challenge_method VARCHAR(20),
+              PRIMARY KEY (authorization_code)
+            );
+
+            CREATE TABLE oauth_refresh_tokens (
+              refresh_token       VARCHAR(40)    NOT NULL,
+              client_id           VARCHAR(80)    NOT NULL,
+              user_id             VARCHAR(80),
+              expires             TIMESTAMP      NOT NULL,
+              scope               VARCHAR(4000),
+              PRIMARY KEY (refresh_token)
+            );
+
+            CREATE TABLE oauth_scopes (
+              scope               VARCHAR(80)  NOT NULL,
+              is_default          BOOLEAN,
+              PRIMARY KEY (scope)
+            );
+
+            CREATE TABLE oauth_jwt (
+              client_id           VARCHAR(80)   NOT NULL,
+              subject             VARCHAR(80),
+              public_key          VARCHAR(2000) NOT NULL
+            );
+
+            CREATE TABLE oauth_jti(
+              issuer              VARCHAR(80)   NOT NULL,
+              subject             VARCHAR(80),
+              audiance            VARCHAR(80),
+              expires             TIMESTAMP     NOT NULL,
+              jti                 VARCHAR(2000) NOT NULL
+            );
+
+            CREATE TABLE oauth_public_keys (
+              client_id            VARCHAR(80),
+              public_key           VARCHAR(2000),
+              private_key          VARCHAR(2000),
+              encryption_algorithm VARCHAR(100) DEFAULT 'RS256'
+            )
+        ";
+        /* @see \OAuth2\Storage\Pdo::class */
+        $this->connection->exec($sql);
+        // Override the bshaffer user claims to fix mapping error for compatibility with OIDC
+        $this->connection->exec('CREATE TABLE oauth_users (username VARCHAR(80), password VARCHAR(80), name VARCHAR(80), family_name VARCHAR(80), email VARCHAR(80), email_verified BOOLEAN, scope VARCHAR(4000))');
         $this->connection->exec('CREATE TABLE oauth_config (parameter VARCHAR(80) PRIMARY KEY, value VARCHAR(8000))');
 
         return $this;
@@ -40,7 +106,7 @@ class TestingDataSet
 
     public function destroy(): self
     {
-        foreach (['oauth_clients', 'oauth_access_tokens', 'oauth_authorization_codes', 'oauth_refresh_tokens', 'oauth_scopes', 'oauth_users', 'oauth_public_keys', 'oauth_config'] as $table) {
+        foreach (['oauth_clients', 'oauth_access_tokens', 'oauth_authorization_codes', 'oauth_refresh_tokens', 'oauth_scopes', 'oauth_users', 'oauth_public_keys', 'oauth_jwt', 'oauth_jti', 'oauth_config'] as $table) {
             $this->connection->exec('DROP TABLE '.$table);
         }
 
@@ -61,12 +127,13 @@ class TestingDataSet
         return $this;
     }
 
-    public function pushClient(string $id, string $secret, string $redirect): self
+    public function pushClient(string $id, string $secret, string $redirect, array $scopes = []): self
     {
         return $this->push('oauth_clients', [
             'client_id' => $id,
             'client_secret' => $secret,
             'redirect_uri' => $redirect,
+            'scope' => $scopes ? implode(' ', $scopes) : null,
         ]);
     }
 
