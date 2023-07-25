82f50451dc524748beaa44a94932b72af70d11aa
Sébastien <stanneux@b2pweb.com>
2023-07-25 11:56:14 +0200

    fix: Fix documentation

--- src/EndPoint/Token/TokenEndPoint.php
+++ src/EndPoint/Token/TokenEndPoint.php
@@ -2,20 +2,16 @@
 
 namespace Parroauth2\Client\EndPoint\Token;
 
-use Http\Client\Exception;
-use Parroauth2\Client\Client;
 use Parroauth2\Client\ClientInterface;
 use Parroauth2\Client\EndPoint\CallableEndPointInterface;
-use Parroauth2\Client\EndPoint\EndPointInterface;
 use Parroauth2\Client\EndPoint\EndPointParametersTrait;
 use Parroauth2\Client\EndPoint\EndPointResponseListenerTrait;
 use Parroauth2\Client\EndPoint\EndPointTransformerInterface;
-use Parroauth2\Client\Exception\Parroauth2Exception;
 
 /**
  * Endpoint for generates an access token
  *
- * @see https://tools.ietf.org/html/rfc6749#section-5.1
+ * @see https://datatracker.ietf.org/doc/html/rfc6749#section-5.1
  *
  * @implements CallableEndPointInterface<TokenResponse>
  */
@@ -85,7 +81,7 @@ class TokenEndPoint implements CallableEndPointInterface
      *
      * @return static
      *
-     * @see https://tools.ietf.org/html/rfc6749#section-4.1.3
+     * @see https://datatracker.ietf.org/doc/html/rfc6749#section-4.1.3
      */
     public function code(string $authorizationCode, ?string $redirectUri = null): TokenEndPoint
     {
@@ -109,7 +105,7 @@ class TokenEndPoint implements CallableEndPointInterface
      *
      * @return static
      *
-     * @see https://tools.ietf.org/html/rfc6749#section-4.3.2
+     * @see https://datatracker.ietf.org/doc/html/rfc6749#section-4.4.2
      *
      * @psalm-mutation-free
      */
@@ -135,7 +131,7 @@ class TokenEndPoint implements CallableEndPointInterface
      *
      * @return static
      *
-     * @see https://tools.ietf.org/html/rfc6749#section-4.3.2
+     * @see https://datatracker.ietf.org/doc/html/rfc6749#section-4.3.2
      *
      * @psalm-mutation-free
      */
@@ -162,7 +158,7 @@ class TokenEndPoint implements CallableEndPointInterface
      *
      * @return static
      *
-     * @see https://tools.ietf.org/html/rfc6749#section-6
+     * @see https://datatracker.ietf.org/doc/html/rfc6749#section-6
      *
      * @psalm-mutation-free
      */
